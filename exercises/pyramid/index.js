1c3d1c849e194223020f80f01188910028bc6d30
Erin Shaw <erin.kimberly.shaw@gmail.com>
2017-12-05 12:33:34 -0500

    finished pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -13,7 +13,39 @@
 //       '  #  '
 //       ' ### '
 //       '#####'
+//   pyramid(4)
+//      '   #   '
+//      '  ###  '
+//      ' ##### '
+//      '#######'
+// 1 - 0
+// 2 - 2
+// 3 - 4
+// 4 - 6
+//spaces are (n - 1) * 2
 
-function pyramid(n) {}
+//inner loop is for rows and equal to n
+//outer loop is (n-1)*2+1 for length
+//if j === n then do a #
+function pyramid(n) {
+  const midpoint = Math.floor((n*2-1)/2)
+  for (let i = 0; i < n; i++) {
+    let row = ''
+    for (let j = 0; j < n*2-1; j++) {
+      if (midpoint - i <= j && midpoint + i >= j) {
+         row += '#'
+      }
+      else {
+        row += ' '
+      }
+    }
+      console.log(row)
+  }
+}
+
+
+//one loop for each row
+//the spaces on first row are equal to n (or n +1)
+//all rows have to be odd
 
 module.exports = pyramid;
